7dbfa455b4fcca426d2aada7a8498dd7424268c0
ganzolder <gnmsapr@gmail.com>
2023-09-17 23:03:06 +0700

    added UI added user interested vacs to save interface added areas name search deleted duplicates in requests bugs fixed

--- json_saver.py
+++ json_saver.py
@@ -8,17 +8,34 @@ class FileSaver:
         self.vac_list_to_save = []
 
     def get_vac_list_to_save(self):
+
+        vacs_urls = []
+        vac_arch = FileSaver.read_from_file()
+
+        for vac in vac_arch:
+            vacs_urls.append(vac[4])
+
         for i in range(len(self.vac_list)):
             i += 1
             if i in self.vac_num_save:
-                self.vac_list_to_save.append(self.vac_list[i - 1])
+                if self.vac_list[i - 1][4] not in vacs_urls:
+                    vac_arch.append(self.vac_list[i - 1])
+                else:
+                    pass
+
+        self.vac_list_to_save = vac_arch
+
         return self.vac_list_to_save
 
     def save_to_file(self):
-        with open('c:/temp/my_list.json', 'r') as file:
-            data = json.load(file)
 
-        print(data)
-
-        with open('c:/temp/my_list.json', 'a') as json_file:
+        with open('c:/temp/my_list.json', 'w') as json_file:
             json.dump(self.vac_list_to_save, json_file)
+
+    @classmethod
+    def read_from_file(cls):
+
+        with open('c:/temp/my_list.json', 'r') as json_file:
+            data = json.load(json_file)
+
+        return data
--- main.py
+++ main.py
@@ -8,23 +8,31 @@ import vac_shorter
 if __name__ == '__main__':
     print('Система поиска работы с максимальной ЗП приветствует тебя, безработный!\n')
 
-    area = input('Введи зону поиска вакансий (Введите "Москва", '
+    start_choice = int(input('Введи цифру для выбора действия:\n'
+          '1. Поиск вакансий\n'
+          '2. Просмотр и редактирование архива ранее сохраненных вакансий\n\n'))
+
+    if start_choice == 1:
+
+        area = input('Введи зону поиска вакансий (Введите "Москва", '
                  'или название любой другой области "замкадья")\n').lower()
+        salary = int(input('Введи размер желаемый минимальный размер оплаты труда\n'))
+        key_word = input('Введи ключевое слово для поиска по вакансиям\n').lower()
+        exp = int(input('Введи свой стаж (в годах)\n'))
+        top_n = int(input('Сколько вывести вакансий?\n'))
 
-    salary = int(input('Введи размер желаемый минимальный размер оплаты труда\n'))
-    key_word = input('Введи ключевое слово для поиска по вакансиям\n').lower()
-    exp = int(input('Введи свой стаж (в годах)\n'))
-    top_n = int(input('Сколько вывести вакансий?\n'))
+        hh_vacs = req_API.HHAPI(area, salary, key_word, exp)
+        vac_list = hh_vacs.get_vac_api_resp()
+        sort_top_n = vac_sorter.HHSorterSalaryTopN(vac_list, salary, top_n).sort_by_salary_top_n()
+        vac_list_shorted = vac_shorter.HHShorter(sort_top_n).get_short_vac()
+        vacs_to_show = printer.ShowFoundedVacs(vac_list_shorted)
+        vacs_to_show.show_vacs_list()
 
-    hh_vacs = req_API.HHAPI(area, salary, key_word, exp)
-    vac_list = hh_vacs.get_vac_api_resp()
-    sort_top_n = vac_sorter.HHSorterSalaryTopN(vac_list, salary, top_n).sort_by_salary_top_n()
-    vac_list_shorted = vac_shorter.HHShorter(sort_top_n).get_short_vac()
-    vacs_to_show = printer.ShowFoundedVacs(vac_list_shorted)
-    vacs_to_show.show_vacs_list()
+        vacs_to_save = list(map(int, input('Выберите номера вакансий чтобы сохранить (через пробел)\n').split()))
 
-    vacs_to_save = list(map(int, input('Выберите номера вакансий чтобы сохранить (через пробел)').split()))
+        list_for_save = json_saver.FileSaver(vac_list_shorted, vacs_to_save)
+        list_for_save.get_vac_list_to_save()
+        list_for_save.save_to_file()
 
-    list_for_save = json_saver.FileSaver(vac_list_shorted, vacs_to_save)
-    list_for_save.get_vac_list_to_save()
-    list_for_save.save_to_file()
+    else:
+        pass
