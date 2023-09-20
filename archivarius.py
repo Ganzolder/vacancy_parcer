576f17b8776076b3f6e098178185a371ece885ab
ganzolder <gnmsapr@gmail.com>
2023-09-21 00:06:20 +0700

    added UI added user interested vacs to save interface added areas name search deleted duplicates in requests bugs fixed added some features

---
+++ archivarius.py
@@ -0,0 +1,36 @@
+import json
+
+
+class Archivist:
+
+    file_path = 'c:/temp/my_list.json'
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def load_archive_file(cls):
+
+        with open(cls.file_path, 'r') as file:
+            data = json.load(file)
+
+        return data
+
+    @classmethod
+    def delete_item(cls, file, item):
+
+        deleting_item = sorted(item, reverse=True)
+        file_to_delete = file
+        i = 0
+
+        for item in deleting_item:
+            item -= 1
+            if i == 0:
+                del file_to_delete[item]
+                pass
+                i += 1
+            else:
+                item -= 1
+                del file_to_delete[item]
+
+        return file_to_delete
--- main.py
+++ main.py
@@ -3,36 +3,74 @@ import req_API
 import printer
 import vac_sorter
 import vac_shorter
+import archivarius
 
 
 if __name__ == '__main__':
-    print('Система поиска работы с максимальной ЗП приветствует тебя, безработный!\n')
 
-    start_choice = int(input('Введи цифру для выбора действия:\n'
-          '1. Поиск вакансий\n'
-          '2. Просмотр и редактирование архива ранее сохраненных вакансий\n\n'))
+    def start_menu():
 
-    if start_choice == 1:
+        print('Система поиска работы с максимальной ЗП приветствует тебя, безработный!\n')
 
-        area = input('Введи зону поиска вакансий (Введите "Москва", '
-                 'или название любой другой области "замкадья")\n').lower()
-        salary = int(input('Введи размер желаемый минимальный размер оплаты труда\n'))
-        key_word = input('Введи ключевое слово для поиска по вакансиям\n').lower()
-        exp = int(input('Введи свой стаж (в годах)\n'))
-        top_n = int(input('Сколько вывести вакансий?\n'))
+        start_choice = int(input('Введи цифру для выбора действия:\n'
+              '1. Поиск вакансий\n'
+              '2. Просмотр и редактирование архива ранее сохраненных вакансий\n'
+              '0. Для завершения работы\n'))
 
-        hh_vacs = req_API.HHAPI(area, salary, key_word, exp)
-        vac_list = hh_vacs.get_vac_api_resp()
-        sort_top_n = vac_sorter.HHSorterSalaryTopN(vac_list, salary, top_n).sort_by_salary_top_n()
-        vac_list_shorted = vac_shorter.HHShorter(sort_top_n).get_short_vac()
-        vacs_to_show = printer.ShowFoundedVacs(vac_list_shorted)
-        vacs_to_show.show_vacs_list()
+        return start_choice
 
-        vacs_to_save = list(map(int, input('Выберите номера вакансий чтобы сохранить (через пробел)\n').split()))
+    start_choice = start_menu()
 
-        list_for_save = json_saver.FileSaver(vac_list_shorted, vacs_to_save)
-        list_for_save.get_vac_list_to_save()
-        list_for_save.save_to_file()
+    while start_choice != 0:
 
-    else:
-        pass
+        match start_choice:
+            case 1:
+                area = input('Введи зону поиска вакансий (Введите "Москва", '
+                         'или название любой другой области "замкадья")\n').lower()
+                salary = int(input('Введи размер желаемый минимальный размер оплаты труда\n'))
+                key_word = input('Введи ключевое слово для поиска по вакансиям\n').lower()
+                exp = int(input('Введи свой стаж (в годах)\n'))
+                top_n = int(input('Сколько вывести вакансий?\n'))
+
+                hh_vacs = req_API.HHAPI(area, salary, key_word, exp)
+                vac_list = hh_vacs.get_vac_api_resp()
+                sort_top_n = vac_sorter.HHSorterSalaryTopN(vac_list, salary, top_n).sort_by_salary_top_n()
+                vac_list_shorted = vac_shorter.HHShorter(sort_top_n).get_short_vac()
+                vacs_to_show = printer.ShowFoundedVacs(vac_list_shorted)
+                vacs_to_show.show_vacs_list()
+
+                vacs_to_save = list(map(int, input('Выберите номера вакансий чтобы сохранить (через пробел)\n').split()))
+
+                list_for_save = json_saver.FileSaver(vac_list_shorted, vacs_to_save)
+                list_for_save.get_vac_list_to_save()
+                list_for_save.save_to_file()
+
+            case 2:
+                vacs_to_delete = 1
+
+                archived_vacs = archivarius.Archivist.load_archive_file()
+
+                while vacs_to_delete != 0:
+                    arch_vacs_to_show = printer.ShowFoundedVacs(archived_vacs)
+                    arch_vacs_to_show.show_vacs_list()
+
+                    vacs_to_delete = list(map(int, input('Выберите номера вакансий чтобы удалить из архива (через пробел)\n'
+                                                         'или введите 0, чтобы вернуться в предыдущее меню\n').split()))
+                    if 0 in vacs_to_delete:
+                        '''vacs_to_save = []
+
+                        for i in range(len(archived_vacs)):
+                            vacs_to_save.append(i)'''
+
+                        #list_for_save = json_saver.FileSaver(archived_vacs, vacs_to_save)
+                        #list_for_save.get_vac_list_to_save()
+                        list_for_save.save_to_file()
+
+                        vacs_to_delete = 0
+                        start_choice = 3
+
+                    else:
+                        archived_vacs = archivarius.Archivist.delete_item(archived_vacs, vacs_to_delete)
+
+            case _:
+                start_choice = start_menu()
